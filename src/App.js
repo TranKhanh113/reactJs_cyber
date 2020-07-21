03eeb7bef131816ebf928640ce396e78cf9ef5a7
Tran Khanh <trankhanh113@gmail.com>
2020-07-21 13:41:15 +0700

    2

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import BaiTapLayout from "./Components/BaiTapLayout/BaiTapLayout";
+import BaiTapLayout2 from "./Components/BaiTapLayout2/BaiTapLayout2";
+import DataBinding from "./DataBinding/DataBinding";
+import EventBinding from "./EventBinding/EventBinding";
+import RenderWithState from "./RenderWithState/RenderWithState";
+import BaiTapChonXe from "./RenderWithState/BaiTapChonXe";
+import RenderWithMap from "./RenderWithState/RenderWithMap/RenderWithMap";
+import ExampleGlasses from "./RenderWithState/ExampleGlasses";
+import DemoProps from "./Props/DemoProps";
+function App() {
+  return (
+    <div className="App">
+      <BaiTapLayout />
+      {/* <BaiTapLayout2 /> */}
+      {/* <DataBinding /> */}
+      {/* <EventBinding /> */}
+      {/* <RenderWithState /> */}
+      {/* <BaiTapChonXe /> */}
+      {/* <RenderWithMap /> */}
+      {/* <ExampleGlasses /> */}
+      <DemoProps />
+    </div>
+  );
+}
+
+export default App;
